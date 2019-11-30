6d29b4d3d646446d9b9a77b026900444dfcc795a
dusankaK <karacdusanka11@gmail>
2019-11-30 19:32:16 +0100

    Zadatak 2

--- src/components/AppPosts.vue
+++ src/components/AppPosts.vue
@@ -1,10 +1,29 @@
 <template>
-    <h1>Neki</h1>
+    <div class="row">
+        <div class="card col-md-4 mt-4" v-for="post in posts" :key="post.id">
+            <div class="card-body">
+                <h5 class="card-title">{{ post.title }}</h5>
+                <p class="card-text">{{ post.text }}</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import { postsService } from '../services/PostsService'
+
 export default {
-    
+    data() {
+        return {
+            posts: Array
+        }
+    },
+
+    created() {
+        postsService.getAll()
+            .then(response =>
+                this.posts = response.data)
+    }
 }
 </script>
 
