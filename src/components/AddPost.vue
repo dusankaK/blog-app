116c318b5a414a73f97ec2a731ed52020496857e
dusankaK <karacdusanka11@gmail>
2019-12-02 11:11:58 +0100

    Zadatak 6

--- src/components/AddPost.vue
+++ src/components/AddPost.vue
@@ -6,11 +6,21 @@
                     <label for="title">Title</label>
                     <input type="title" class="form-control" id="title" aria-describedby="emailHelp" 
                     placeholder="Title" v-model="newPost.title">
+
+                    <div v-if="errors.title" class="alert alert-danger">
+                        <p>Post title is required with at least 2 characters.</p>
+                    </div>
+
                 </div>
                 <div class="form-group">
-                    <label for="title">Write post</label>
+                    <label for="text">Write post</label>
                     <textarea rows="6" cols="50" class="form-control" id="text"
                     placeholder="Write post..." v-model="newPost.text"></textarea>
+
+                    <div v-if="errors.text" class="alert alert-danger">
+                        <p>Post body is limited to 300 characters.</p>
+                    </div>
+
                 </div>
                 <div class="form-group">
                     <button type="submit" name="submit" class="btn btn-success">Submit</button>
@@ -30,19 +40,44 @@ import { postsService } from '../services/PostsService'
 export default {
     data() {
         return {
-            newPost: {}
+            newPost: {},
+            errors: {
+                title: false,
+                text: false
+            }
         }
     },
 
     methods: {
         onSubmit() {
+            if(this.hasErrors()) {
+                return
+            }
             this.newPost.createdAt = moment()
             postsService.add(this.newPost)
-            this.$router.push('/posts')
+            this.$router.push({ name: 'posts'})
         },
 
         resetForm() {
             this.newPost = {}
+        },
+
+        hasErrors() {
+            let errorCount = 0
+            if (!this.newPost.title || this.newPost.title.length < 2) {
+                this.errors.title = true
+                errorCount++ 
+            } else { 
+                this.errors.title = false
+                }
+            if (!this.newPost.text || this.newPost.text.length > 300) {
+                this.errors.text = true
+                errorCount++ 
+            } else {
+                this.errors.text = false 
+                }
+
+            return errorCount
         }
     }
 }
